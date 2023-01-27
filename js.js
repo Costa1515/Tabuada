34d2f9716ea5e7acb0ff247af67e4ec29fbd2f59
Costa1515 <116725667+Costa1515@users.noreply.github.com>
2023-01-27 10:31:32 +0000

    refactor

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 const multiplicate = document.getElementById("button");
 const input = document.getElementById("input");
-const table = document.getElementsByTagName("table");
-const div = document.getElementById("table1");
+const table = document.getElementById("table");
+const form = document.getElementById("form");
 let result = 1;
 
-div.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
@@ -12,6 +12,29 @@ multiplicate.addEventListener("click", () => {
     for (let i = 0; i <= 12; i++) {
         result = input.value * i;
         console.log(result);
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const td4 = document.createElement("td");
+        const td5 = document.createElement("td");
+
+        td1.innerText = input.value;
+        td2.innerText = "X";
+        td3.innerText = i;
+        td4.innerText = "=";
+        td5.innerText = result;
+
+        table.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+
+
+
+
     }
 
 })
